53af89a8845147c80b788c8540a14557cec2e612
eric-yates <eric@lumenalabs.com>
2024-01-29 21:22:35 -0800

    patch: export types

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@
 export { default as SubplotGrapher } from './SubplotGrapher'
 export * from './SubplotGrapher'
 
+// Types
+
+export * from './types/nodeServerPlots.types'
+export * from './types/chartJSNodeCanvas.types'
+export * from './types/sharp.types'
+
 // Test doubles
 
 export { default as FakeChartJSNodeCanvas } from './testDoubles/FakeChartJSNodeCanvas'
