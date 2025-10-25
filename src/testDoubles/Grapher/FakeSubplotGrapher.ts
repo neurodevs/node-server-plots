45782c740d35f362884a7c66032cfb7811132844
ericthecurious <hello@ericthecurious.com>
2025-10-25 00:11:24 -0700

    patch: update fake

--- src/testDoubles/Grapher/FakeSubplotGrapher.ts
+++ src/testDoubles/Grapher/FakeSubplotGrapher.ts
@@ -5,14 +5,19 @@ import {
 } from '../../types/nodeServerPlots.types'
 
 export default class FakeSubplotGrapher implements Grapher {
-    public static constructorCalledWith: SubplotGrapherOptions[] = []
-    public static generateCalledWith: GrapherGenerateOptions[] = []
+    public static callsToConstructor: SubplotGrapherOptions[] = []
+    public static callsToGenerate: GrapherGenerateOptions[] = []
 
     public constructor(options: SubplotGrapherOptions) {
-        FakeSubplotGrapher.constructorCalledWith.push(options)
+        FakeSubplotGrapher.callsToConstructor.push(options)
     }
 
     public async generate(options: GrapherGenerateOptions) {
-        FakeSubplotGrapher.generateCalledWith.push(options)
+        FakeSubplotGrapher.callsToGenerate.push(options)
+    }
+
+    public static resetTestDouble() {
+        FakeSubplotGrapher.callsToConstructor = []
+        FakeSubplotGrapher.callsToGenerate = []
     }
 }
